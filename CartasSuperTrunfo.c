0e944d15a8c73de5cae04c5c4409fc6f41e37d18
Regis Silvestre <regis-silvestre@hotmail.com>
2025-08-25 23:28:04 +0000

    Fianalizando Desafio Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,14 +2,17 @@
 
 
 int main() {
-    
+    //Declar as variáveis
     char estado1, estado2;
     char codcarta1[4], codcarta2[4], nomecid1[50], nomecid2[50];
-    int populacao1, populacao2, numpt1, numpt2;
+    int numpt1, numpt2;
     float aream21, aream22, pib1, pib2, densPop1, densPop2, pibPCap1, pibPCap2;
-    
-   
-        printf("******CARTA 1****** \n"); // Primeira carta trunfo.
+    unsigned long int populacao1, populacao2, resultadoPop;
+    float superPoderCarta1, superPoderCarta2;
+    int resultadoArea, resultadoPib, resultadoNumpt, resultadoDensPop, resultadoPibPcap, resultadoSuperPoder;
+
+        //Entrada de dados da primeira Carta.
+        printf("******CARTA 1****** \n"); 
         
         printf("Digite a primeira letra do Estado de A a H: \n");
         scanf(" %c", &estado1);
@@ -22,7 +25,7 @@ int main() {
         fgets(nomecid1, 50, stdin);
            
         printf("Digite a população desta cidade: \n");
-        scanf(" %d", &populacao1);
+        scanf(" %lu", &populacao1);
     
         printf("Digite a área da cidade em metros quadrados: \n");
         scanf(" %f", &aream21);
@@ -32,12 +35,13 @@ int main() {
 
         printf("Digite o número de pontos turisticos desta cidade: \n");
         scanf(" %d", &numpt1);
-
+        //Calculando a Densidade Polulacional da primeira Carta
         densPop1 = populacao1 / aream21;
+        //Calculando o PIB per Capita da primeira Carta
         pibPCap1 = pib1 / populacao1;
 
-
-                printf("******CARTA 2****** \n"); // Segunda carta trunfo.
+                // Entrada de dados da segunda Carta.
+                printf("******CARTA 2****** \n");
         
                 printf("Digite a primeira letra do Estado de A a H: \n");
                 scanf(" %c", &estado2);
@@ -46,11 +50,11 @@ int main() {
                 scanf(" %s", codcarta2);
 
                 printf("Digite o nome da cidade: \n");
-                getchar();// usado para não pular linha e poder colocar o nome da cidade
+                getchar();
                 fgets(nomecid2, 50, stdin);
                                        
                 printf("Digite a população desta cidade: \n");
-                scanf(" %d", &populacao2);
+                scanf(" %lu", &populacao2);
             
                 printf("Digite a área da cidade em metros quadrados: \n");
                 scanf(" %f", &aream22);
@@ -61,32 +65,63 @@ int main() {
                 printf("Digite o número de pontos turísticos desta cidade: \n");
                 scanf(" %d", &numpt2);
 
+                //Calculando a Densidade Polulacional da segunda Carta
                 densPop2 = populacao2 / aream22;
+                //Calculando o PIB per Capita da segunda Carta
                 pibPCap2 = pib2 / populacao2;
-                
-                printf("\n"); // usado para pular linha
-    
-        printf("******CARTA 1******\n"); // Primeira carta trunfo.
+
+        /* Somando todos os atributos numéricos (população, área, PIB, número de pontos turísticos, 
+        PIB per capita e o inverso da densidade populacional – quanto menor a densidade, maior o "poder")*/
+        superPoderCarta1 = (float) populacao1 + aream21 + pib1 + numpt1 + pibPCap1 + (1 / densPop1);
+        superPoderCarta2 = (float) populacao2 + aream22 + pib2 + numpt2 + pibPCap2 + (1 / densPop2);
+         
+    // Comparação das Cartas
+    resultadoPop = populacao1 > populacao2;
+    resultadoArea = aream21 > aream22;
+    resultadoPib = pib1 > pib2;
+    resultadoNumpt = numpt1 > numpt2;
+    resultadoDensPop = densPop1 < densPop2; //vence a menor densidade
+    resultadoPibPcap = pibPCap1 > pibPCap2;
+    resultadoSuperPoder = superPoderCarta1 > superPoderCarta2;
+        
+        printf("\n");
+        
+        //Exibir atributos da primeira Carta
+        printf("******CARTA 1******\n"); 
         printf("Estado: %c\n", estado1);
         printf("Código: %s\n", codcarta1);
         printf("Nome da Cidade: %s", nomecid1);   
-        printf("População: %d\n", populacao1);
+        printf("População: %lu\n", populacao1);
         printf("Área: %.2f km² \n", aream21);
         printf("PIB: %.2f bilhões de reais \n", pib1); 
         printf("Números de Pontos Turisticos: %d \n", numpt1);
         printf("Densidade Populacional: %.2f hab/km² \n", densPop1);
         printf("PIB per Capita: %.2f reais \n\n", pibPCap1);
 
-                printf("******CARTA 2****** \n"); // Primeira carta trunfo.
+                // Exibir atributos da segunda Carta
+                printf("******CARTA 2****** \n");
                 printf("Estado: %c\n", estado2);
                 printf("Código: %s\n", codcarta2);
                 printf("Nome da Cidade: %s", nomecid2);   
-                printf("População: %d\n", populacao2);
+                printf("População: %lu\n", populacao2);
                 printf("Área: %.2f km² \n", aream22);
                 printf("PIB: %.2f bilhões de reais \n", pib2); 
                 printf("Números de Pontos Turisticos: %d \n", numpt2);
                 printf("Densidade Populacional: %.2f hab/km² \n", densPop2);
                 printf("PIB per Capita: %.2f reais \n", pibPCap2);
+        
+                printf("\n");
+        
+        //Exibir resultado das comparações das Cartas
+        // Resposta 1 - vence Carta 1 e Resposta 0 - vence Carta 2 
+        printf("******RESULTADO DA COMPARAÇÃO DAS CARTAS******\n");
+        printf("População: Carta 1 venceu Carta 2? Resposta: %d\n", resultadoPop);
+        printf("Área: Carta 1 venceu Carta 2? Resposta: %d\n", resultadoArea);
+        printf("PIB: Carta 1 venceu Carta 2? Resposta: %d\n", resultadoPib);
+        printf("Número de Pontos Turisticos: Carta 1 venceu Carta 2? Resposta: %d\n", resultadoNumpt);
+        printf("Densidade Populacional: Carta 1 venceu Carta 2? Resposta: %d\n", resultadoDensPop);
+        printf("PIB per Capita: Carta 1 venceu Carta 2? Resposta: %d\n", resultadoPibPcap);
+        printf("Super Poder: Carta 1 venceu Carta 2? Resposta: %d\n", resultadoSuperPoder);
 
 
     return 0;
